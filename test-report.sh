bcdc33558e571b736e70706fae4cb3a47ee040aa
andreformento <andreformento.sc@gmail.com>
2017-11-24 20:47:19 +0000

    Fix health endpoint

--- test-report.sh
+++ test-report.sh
@@ -6,11 +6,11 @@
 docker-compose up -d
 
 printf 'Waiting for application is ready'
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080/health); do
+until $(curl --output /dev/null --silent --head --fail http://localhost:8080/application); do
     printf '.'
     sleep 0.1
 done
 
-curl -X POST 'http://localhost:8080/event-reservations' -H 'Content-Type: application/json' -d '{"eventId": "uuid456", "limit": 90005}'
+curl -v -w '%{time_total}\n' -X POST 'http://localhost:8080/event-reservations' -H 'Content-Type: application/json' -d '{"eventId": "uuid456", "limit": 90005}'
 docker-compose --file docker-compose-test.yml up
 docker-compose down
