983afb11bd939bf383eac56989e50d3cd4be51e7
andreformento <andreformento.sc@gmail.com>
2017-11-24 13:00:04 +0000

    Fix script wait

--- test-report.sh
+++ test-report.sh
@@ -2,7 +2,13 @@
 
 docker-compose down
 docker-compose up -d
-sleep 4
+
+printf 'Waiting for application is ready'
+until $(curl --output /dev/null --silent --head --fail http://localhost:8080/health); do
+    printf '.'
+    sleep 0.1
+done
+
 curl -X POST 'http://localhost:8080/event-reservations' -H 'Content-Type: application/json' -d '{"eventId": "uuid456", "limit": 90005}'
 docker-compose --file docker-compose-test.yml up
 docker-compose down
