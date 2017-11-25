ae3aa7d829c0a221622f8d1bb9559d471242a87a
André Formento <andreformento.sc@gmail.com>
2017-11-25 21:01:56 -0200

    Update script

--- test-report.sh
+++ test-report.sh
@@ -37,16 +37,16 @@ fi
 $startApplication
 
 printf 'Waiting for application is ready'
-until $(curl --output /dev/null --silent --head --fail $host:8080/application); do
+until $(curl --output /dev/null --silent --head --fail $host/application); do
     printf '.'
     sleep 0.1
 done
 
 printf '\n'
-curl -v -w '\n%{time_total}\n' -X POST '$host:8080/event-reservations' -H 'Content-Type: application/json' -d '{"eventId": "uuid456", "limit": 90005}'
+curl -v -w '\n%{time_total}\n' -X POST '$host/event-reservations' -H 'Content-Type: application/json' -d '{"eventId": "uuid456", "limit": 90005}'
 printf '\n'
 
-export JAVA_OPTS_TEST="-Dhostname_test=$host:8080"
+export JAVA_OPTS_TEST="-Dhostname_test=$host"
 docker-compose --file docker-compose-test.yml up
 
 ./clean.sh
