3a8b37ef88870f990206ae478f0f596fa36426c4
André Formento <andreformento.sc@gmail.com>
2017-11-25 21:54:17 -0200

    constantUsersPerSec(3000) during(5 /*seconds*/)

--- ticket-reservation-gatling/user-files/simulations/computerdatabase/BasicSimulation.scala
+++ ticket-reservation-gatling/user-files/simulations/computerdatabase/BasicSimulation.scala
@@ -36,7 +36,7 @@ class BasicSimulation extends Simulation { // 3
 
   setUp(
     scn.inject(
-        constantUsersPerSec(2000) during(5 /*seconds*/)
+        constantUsersPerSec(3000) during(5 /*seconds*/)
         // heavisideUsers(3000) over(5 /*seconds*/)
       )
   ).protocols(httpConf) // 13
